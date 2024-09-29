1700a479efdbae10da804d0fc4939d8882dff98d
MichaelScofield45 <paco.villamonte@gmail.com>
2024-09-29 13:43:55 -0600

    Fix removal of single quote from number layer

--- keyboards/ferris/keymaps/ms45/keymap.c
+++ keyboards/ferris/keymaps/ms45/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LBRC,  KC_7,     KC_8,     KC_9,     KC_RBRC,   XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,
         KC_MINUS, KC_4,     KC_5,     KC_6,     KC_EQL,    XXXXXXX, KC_LSFT,  KC_LCTL,  KC_LALT,   KC_LGUI,
         KC_GRAVE, KC_1,     KC_2,     KC_3,     KC_BSLS,   XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,
-                                       _______, _______,   _______, _______
+                                       _______, KC_QUOT,   _______, _______
     ),
 	[SYM] = LAYOUT_split_3x5_2(
         KC_LCBR,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RCBR,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
