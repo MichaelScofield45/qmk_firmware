7b3a7d922a73cfcd842dce38e6b9fc40304f1265
MichaelScofield45 <paco.villamonte@gmail.com>
2024-12-05 10:17:42 -0600

    Fix double quotes not working with modifier r_alt key

--- keyboards/ferris/keymaps/ms45/keymap.c
+++ keyboards/ferris/keymaps/ms45/keymap.c
@@ -36,16 +36,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 	[NUM] = LAYOUT_split_3x5_2(
 
-        KC_LBRC,  KC_7,     KC_8,     KC_9,     KC_RBRC,   XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,
-        KC_MINUS, KC_4,     KC_5,     KC_6,     KC_EQL,    XXXXXXX, KC_LSFT,  KC_LCTL,  KC_LALT,   KC_LGUI,
-        KC_GRAVE, KC_1,     KC_2,     KC_3,     KC_BSLS,   XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,
-                                      KC_0,     KC_QUOT,   XXXXXXX, XXXXXXX
+        KC_LBRC,  KC_7,     KC_8,     KC_9,     KC_RBRC,           XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,
+        KC_MINUS, KC_4,     KC_5,     KC_6,     KC_EQL,            XXXXXXX, KC_LSFT,  KC_LCTL,  KC_LALT,   KC_LGUI,
+        KC_GRAVE, KC_1,     KC_2,     KC_3,     KC_BSLS,           XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,
+                                      KC_0,     RALT_T(KC_QUOT),   XXXXXXX, XXXXXXX
     ),
 	[SYM] = LAYOUT_split_3x5_2(
-        KC_LCBR,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RCBR,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        KC_UNDS,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_PLUS,   XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
-        KC_TILD,  KC_EXLM,  KC_AT,    KC_HASH,  KC_PIPE,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                      KC_RPRN,  KC_DQUO,   XXXXXXX, XXXXXXX
+        KC_LCBR,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RCBR,           XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_UNDS,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_PLUS,           XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
+        KC_TILD,  KC_EXLM,  KC_AT,    KC_HASH,  KC_PIPE,           XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                                      KC_RPRN,  KC_DQUO,           XXXXXXX, XXXXXXX
     ),
 	[NAV] = LAYOUT_split_3x5_2(
         XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX, KC_PSTE, KC_COPY, KC_CUT,  KC_UNDO,
